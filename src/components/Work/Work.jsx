2f428eec5e25b80ef04bd5112f77db5b834ddf8b
Eldho@1802 <eldhotbabu12@gmail.com>
2025-05-10 19:19:20 +0530

    project

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -21,10 +21,7 @@ const Work = () => {
       <div className="text-center mb-16">
         <h2 className="text-4xl font-bold text-white">PROJECTS</h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
-        <p className="text-gray-400 mt-4 text-lg font-semibold">
-          A showcase of the projects I have worked on, highlighting my skills
-          and experience in various technologies
-        </p>
+        
       </div>
 
       {/* Projects Grid */}
@@ -111,7 +108,7 @@ const Work = () => {
                   >
                     View Code
                   </a>
-              
+                 
                 </div>
               </div>
             </div>
